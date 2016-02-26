70252f62bf8e83fc9d62200cd528102155025468
Danielle Madeley <danielle.madeley@squareweave.com.au>
2016-02-26 14:47:44 +1100

    Add README to long_description

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="django_loaddata_stdin",
-    version="0.0.3",
-    description="Django module to load data from stdin",
-    url='https://github.com/squareweave/django-loaddata-stdin',
-    author='Squareweave',
-    author_email='team@squareweave.com.au',
-    license='BSD',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    packages=find_packages(),
-    install_requires=['django >= 1.8'],
-)
+with open('README.md') as readme, \
+     open('requirements.txt') as requirements:
+    setup(
+        name="django_loaddata_stdin",
+        version="0.0.4",
+        description="Django module to load data from stdin",
+        long_description=readme.read(),
+        url='https://github.com/squareweave/django-loaddata-stdin',
+        author='Squareweave',
+        author_email='team@squareweave.com.au',
+        license='BSD',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+        packages=find_packages(),
+        install_requires=requirements.readlines(),
+    )
