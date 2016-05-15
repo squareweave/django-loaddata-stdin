87d155f6cd5e46a09fab7748a7419e506a8ce78a
ZuluPro <montheanthony@hotmail.com>
2016-05-15 06:55:39 -0400

    Added unit test and Python 2 compat

---
+++ django_loaddata_stdin/tests/settings.py
@@ -0,0 +1,27 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ADMIN = ('foo@bar')
+ALLOWED_HOSTS = ['*']
+MIDDLEWARE_CLASSES = ()
+ROOT_URLCONF = 'django_loaddata_stdin.tests.testapp.urls'
+SECRET_KEY = "it's a secret to everyone"
+SITE_ID = 1
+INSTALLED_APPS = (
+    'django_loaddata_stdin',
+    'django_loaddata_stdin.tests.testapp',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
---
+++ django_loaddata_stdin/tests/test_command.py
@@ -0,0 +1,22 @@
+from mock import patch
+
+from django.test import TestCase
+from django.utils.six import BytesIO
+from django.core.management import execute_from_command_line
+
+from django_loaddata_stdin.management.commands.loaddata import Command
+from django_loaddata_stdin.tests.testapp.models import CharModel
+
+YAML_FIXTURE = b"""
+- fields: {field: a}
+  model: testapp.charmodel
+  pk: 1
+"""
+
+
+@patch('django_loaddata_stdin.management.commands.loaddata.sys.stdin', BytesIO(YAML_FIXTURE))
+class LoadDataTest(TestCase):
+    def test_command(self):
+        argv = ['', 'loaddata', '--format=yaml', '-']
+        execute_from_command_line(argv)
+        self.assertTrue(CharModel.objects.exists())
